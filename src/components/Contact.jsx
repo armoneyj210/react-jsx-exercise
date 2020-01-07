ec42f426305a9b6a97fc58bc87100ccb0fe86ba3
Joshua Armoney <armoneyj210@gmail.com>
2020-01-07 07:37:35 -0500

    nav bar jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className="contact">
         <h1>Store Hours</h1>
         <p>
           <strong>Sunday-Thursday:</strong>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class Navbar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="navbar">
+        <a href="#description">OurStory</a>
+        <a href="#menu">Menu</a>
+        <h1>PIZZA-EXPRESS</h1>
+        <a href="#locations">Locations</a>
+        <a href="#contact">Contact</a>
+      </div>
+    );
   }
 }
