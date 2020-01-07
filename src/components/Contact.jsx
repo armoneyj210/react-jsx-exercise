fc6f5f17ea2bd1cf7b387f1ddf26808da2b0973d
Joshua Armoney <armoneyj210@gmail.com>
2020-01-07 07:22:42 -0500

    contact jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 
 export default class Contact extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Store Hours</h1>
+        <p>
+          <strong>Sunday-Thursday:</strong>
+          "11:00am to 12:00"
+        </p>
+        <div>
+          <p>
+            <strong>Phone:</strong>
+            "404-555-1234"
+          </p>
+          <p>
+            <strong>Email:</strong>
+            "pizzaheaven@yes.please"
+          </p>
+        </div>
+      </div>
+    );
   }
 }
